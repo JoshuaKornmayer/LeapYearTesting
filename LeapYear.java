1916eda237b9cd40ba5bc7f7fc1c82074dfa9f1f
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-06-01 17:04:26 -0400

    create the isleapyear method with modified if statement

--- LeapYear.java
+++ LeapYear.java
@@ -1,24 +1,50 @@
 package Leapyear;
 
 public class LeapYear {
-    private long year;
+    private int year;
+    static boolean isLeapYear;
 
     // constructor
-    public LeapYear(long year) {
-        this.year = year;
-    }
-
-    public boolean isLeapYear() {
-        return isDivisibleBy4();
+    public LeapYear() {
 
     }
 
-    public boolean isDivisibleBy4() {
-        return ((year % 4 == 0) && isDivisibleBy400());
+    public int getLeapYear() {
+        return this.year;
     }
 
-    public boolean isDivisibleBy400() {
-        return true;
+    public static int yearlength(int year) {
+        int yearLength = String.valueOf(year).length();
+        if (yearLength > 4) {
+            throw new RuntimeException("Years can only be 4 numbers.");
+        }
+        return yearLength;
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (isDivisibleBy4(year)) {
+            if (!isDivisibleBy100(year)) {
+                isLeapYear = true;
+            } else if (isDivisibleBy400(year)) {
+                isLeapYear = true;
+            }
+        } else {
+            isLeapYear = false;
+        }
+
+        return isLeapYear;
+    }
+
+    public static boolean isDivisibleBy4(int year) {
+        return year % 4 == 0;
+    }
+
+    public static boolean isDivisibleBy400(int year) {
+        return year % 400 == 0;
+    }
+
+    public static boolean isDivisibleBy100(int year) {
+        return year % 100 == 0;
     }
 
 }
--- LeapYearTest.java
+++ LeapYearTest.java
@@ -1,22 +1,42 @@
 package Leapyear;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class LeapYearTest {
 
+    @Test(expected = RuntimeException.class)
+    public final void whenMoreThan4NumbersAreUsedForAYearThenAnExceptionIsThrown() {
+        LeapYear.yearlength(20135);
+    }
+
     @Test
     public void isTheGivenYearALeapYear() {
-        LeapYear leapyear = new LeapYear(2013);
+        // LeapYear leapyear = new LeapYear(2013);
 
-        assertTrue(leapyear.isLeapYear());
+        assertTrue(LeapYear.isLeapYear(2016));
     }
 
     @Test
     public void isTheGivenYearDivisibleBy4() {
-        LeapYear leapyear = new LeapYear(2013);
+        // LeapYear leapyear = new LeapYear(2013);
 
-        assertTrue(leapyear.isDivisibleBy4());
+        assertTrue(LeapYear.isDivisibleBy4(2016));
+    }
+
+    @Test
+    public void isTheGivenYearDivisibleBy400() {
+        // LeapYear leapyear = new LeapYear(2013);
+
+        assertTrue(LeapYear.isDivisibleBy400(2000));
+    }
+
+    @Test
+    public void isTheGivenYearNotDivisibleBy100() {
+        // LeapYear leapyear = new LeapYear(2013);
+
+        assertEquals(true, LeapYear.isDivisibleBy100(2000));
     }
 }
